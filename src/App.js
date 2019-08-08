8efdf917218a3fcb2e333a2797a9070d7b751bfe
marycriv <maryecrivelli@gmail.com>
2019-08-08 15:36:28 -0400

    Added login form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TopBar from './header/TopBar'
-import MainContainer from './main/MainContainer'
+import TopBar from './constant/header/TopBar';
+import MainContainer from './constant/main/MainContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <TopBar />
-      <MainContainer />
-    </div>
-  );
-}
+const API = "http://localhost:3001"
 
-export default App;
+export default class App extends React.Component {
+  state = {
+    users: [],
+    items: [],
+    currentUserId: null
+  }
+
+  componentDidMount(){
+    this.getUsers()
+    this.getItems()
+  }
+
+  getUsers = () => {
+    fetch(`${API}/users`)
+    .then(resp => resp.json())
+    .then(users => this.setState({
+      users: users
+    }, console.log(this.state.users)))
+  }
+
+  getItems = () => {
+    fetch(`${API}/items`)
+    .then(resp => resp.json())
+    .then(items => this.setState({
+      items: items
+    }, console.log(this.state.items)))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <TopBar />
+        <MainContainer currentUserId={this.state.currentUserId}
+        users={this.state.users}
+        items={this.state.items}
+        />
+      </div>
+    );
+  }
+} // end app class
