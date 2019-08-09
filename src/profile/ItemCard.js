6b557b1c13d09d9269d68ce10fa42504127c3263
marycriv <maryecrivelli@gmail.com>
2019-08-09 11:38:21 -0400

    ItemCard adjusted

--- src/profile/ItemCard.js
+++ src/profile/ItemCard.js
@@ -12,7 +12,17 @@ const ItemCard = (props) => {
   return (
     <div className="ItemCard">
       {items.map((item) => {
-        return (<img width="250px" alt={`${item.user_id}-${item.brand}`} key={`${item.brand}-${item.id}`} src={item.image} />)
+        return (
+          <div className="ItemCard">
+            <img width="250px" alt={`${item.user_id}-${item.brand}`} key={`${item.brand}-${item.id}`} src={item.image} />
+            <ul>
+              <li>Id: {item.id}</li>
+              <li>Brand: {item.brand}</li>
+              <li>Classification: {item.classification}</li>
+              <li>User id: {props.user.username}</li>
+            </ul>
+          </div>
+      )
       })}
     </div>
   )
@@ -23,6 +33,7 @@ function msp(state){
   return {
     currentUserId: state.currentUserId,
     users: state.users,
+    user: state.user,
     items: state.items
   }
 }
