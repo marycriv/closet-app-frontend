cf3215da05b92ce792ae9b3968b475f46cf6d2e9
marycriv <maryecrivelli@gmail.com>
2019-08-16 15:59:34 -0400

    Broke everything by trying to add CSS

--- src/FollowsContainer.js
+++ src/FollowsContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
 import { connect } from 'react-redux';
 
 class FollowsContainer extends React.Component{
@@ -11,9 +13,9 @@ class FollowsContainer extends React.Component{
 
     return(
       <div className="FollowsContainer">
-        {this.props.followersToggle ? <div><h3>Followers:</h3>{followers.map(follower => <div className="SingleFollow"><img width="100px" src={follower.profile_picture}/><h1>{follower.username}</h1></div>)}</div> : null}
+        {this.props.followersToggle ? <div><h3>Followers:</h3>{followers.map(follower => <div className="SingleFollow"><Link to={`/${follower.username}`}><img width="100px" src={follower.profile_picture}/></Link><h1>{follower.username}</h1></div>)}</div> : null}
 
-        {this.props.followingToggle ? <div><h3>Following:</h3>{followees.map(followee => <div className="SingleFollow"><img width="100px" src={followee.profile_picture}/><h1>{followee.username}</h1></div>)}</div> : null}
+        {this.props.followingToggle ? <div><h3>Following:</h3>{followees.map(followee => <div className="SingleFollow"><Link to={`/${followee.username}`}><img width="100px" src={followee.profile_picture}/></Link><h1>{followee.username}</h1></div>)}</div> : null}
       </div>
     )
   }
