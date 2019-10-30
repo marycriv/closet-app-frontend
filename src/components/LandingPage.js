8d1abbcfe2d42aac2dbec38c4f3d4de58fa4c873
marycriv <maryecrivelli@gmail.com>
2019-10-29 22:41:59 -0400

    Welcome message draft

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,10 @@ import React from 'react';
 import './../main.css'
 
 const LandingPage = () => {
-  return <div className="LandingPage"><center><img width="300px" alt="logo" src='/logo.png' /></center></div>
+  return <div>
+  <p>Welcome.</p>
+  <br/>
+  <div className="LandingPage"><center><img width="300px" alt="logo" src='/logo.png' /></center></div></div>
 }
 
 export default LandingPage;
