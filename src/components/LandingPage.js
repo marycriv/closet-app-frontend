130ab8f1da32d6b745ef0e696c9c95f7bcdd4959
marycriv <maryecrivelli@gmail.com>
2019-10-29 23:45:54 -0400

    Login link not working - removed for now

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,7 @@ import './../main.css'
 
 const LandingPage = () => {
   return <div>
-  <div className="welcomeHeading">Welcome to MissMatched. Please <a href="/login">go to the login page</a> and log in as the demo user "elle", with the credentials provided.</div>
+  <div className="welcomeHeading">Welcome to MissMatched. Please go to the login page above and log in as the demo user "elle", with the credentials provided.</div>
   <div className="LandingPage"><center><img width="300px" alt="logo" src='/logo.png' /></center></div></div>
 }
 
